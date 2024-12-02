85dc5738eea274a6d7d6bfae6cb709c37748c93e
engnn <engnsrgn@gmail.com>
2024-12-02 15:26:30 +0300

    giris yazisi engin tarafından degistirildi

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Archi's Academy Github Kursuna Hoşgeldiniz!
+          Git & Github ögreniyorum w/engin
           
         </p>
         <a
