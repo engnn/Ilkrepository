96fbc6565e818c098461ee044986502cc323c08f
engnn <engnsrgn@gmail.com>
2024-12-08 14:00:25 +0300

    hosgeldiniz yazisi eklendi

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          HOŞGELDİNİZ
+          </p>
+          <p>
           Git & Github ögreniyorum w/engin
           
         </p>
